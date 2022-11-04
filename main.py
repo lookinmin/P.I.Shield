0a60a5b9c814dd57f15a7c754fd4bf8ce19ede2a
minwg1021 <minwg1021@gmail.com>
2022-11-04 21:17:33 +0900

    정규식 수정

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import cv2
+import numpy as np
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 import easyocr
@@ -20,16 +22,19 @@ def func(filenametemp):
     # 텍스트 찾는 AI
     text_detect_result = reader.detect(filenametemp, min_size=3, text_threshold=0.1, low_text=0.4, link_threshold=0.2,
                                width_ths=0.5) #최소사이즈, 텍스트 최소인식률, 작아질수록 인식 업 , ,박스 합쳐지는 범위
+
     # 찾은 텍스트를 txt 포맷으로 변경
     result = reader.recognize(filenametemp, text_detect_result[0][0],text_detect_result[1][0])
 
     registNum = '(\d{6}[ ,-]-?[1-4]\d{6})|(\d{6}[ ,-]?[1-4])'
-    name = '^[가-힣]{2,3}$'
+    name = '(^[가-힣]{2,3}$)'
     account = '([0-9,\-]{3,6}\-[0-9,\-]{2,6}\-[0-9,\-])'
-    address1 = '(([가-힣A-Za-z·\d~\-\.]{2,}(로|길).[\d]+)|([가-힣A-Za-z·\d~\-\.]+(읍|동)\s)[\d]+)'
-    address2 = '(([가-힣A-Za-z·\d~\-\.]+(읍|동)\s)[\d-]+)|(([가-힣A-Za-z·\d~\-\.]+(읍|동)\s)[\d][^시]+)'
-    phone = '(\d{2,3}[ ,-]-?\d{2,4}[ ,-]-?\d{4})'
+    address1 = '(([가-힣A-Za-z·\d~\-\.\[\]\(\)]{2,}(로|길).[\d]+)|([가-힣A-Za-z·\d~\-\.\[\]\(\)]+(읍|동)\s)[\d]+)'
+    address2 = '(([가-힣A-Za-z·\d~\-\.\[\]\(\)]+(읍|동)\s)[\d-]+)|(([가-힣A-Za-z·\d~\-\.\[\]\(\)]+(읍|동)\s)[\d][^시]+)'
+    phone = '([O\d]{2,3}[ ,-]-?\d{2,4}[ ,-]-?\d{4})'
     email = '(([\w!-_\.])*@([\w!-_\.]))'
+    carNum = '^[가-힣]{2}\d{2}[가-힣]{1}\d{4}$'
+    carNum2 = '^\d{2}[가-힣]{1}\d{4}$'
 
     img = Image.open(filenametemp)
     font = ImageFont.truetype('malgun.ttf', 50) #폰트 지정
@@ -44,9 +49,9 @@ def func(filenametemp):
         draw.polygon(temp_xy, fill=fill)
 
     for i in result:
-        print(i[0],i[1],i[2])
+        print(i[0],str(i[1]).replace(' ',''),i[2])
 
-        if re.match(registNum, str(i[1])) != None:
+        if re.match(registNum, str(i[1]).replace(' ','')) != None:
             mask = Image.new('L', img.size, 0)
             draw = ImageDraw.Draw(mask)
             rounded_rectangle(draw, i[0], rad=5, fill=255)
@@ -54,15 +59,7 @@ def func(filenametemp):
             blurred = img.filter(ImageFilter.GaussianBlur(20))
             # Paste blurred region and save result
             img.paste(blurred, mask=mask)
-            # elif re.match(name, str(i[1])) != None:
-            #     mask = Image.new('L', img.size, 0)
-            #     draw = ImageDraw.Draw(mask)
-            #     rounded_rectangle(draw, (x, y, x+w, y+h), rad=40, fill=255)
-            #     # Blur image
-            #     blurred = img.filter(ImageFilter.GaussianBlur(20))
-            #     # Paste blurred region and save result
-            #     img.paste(blurred, mask=mask)
-        elif re.match(account, str(i[1])) != None:
+        elif re.match(account, str(i[1]).replace(' ','')) != None:
             mask = Image.new('L', img.size, 0)
             draw = ImageDraw.Draw(mask)
             rounded_rectangle(draw, i[0], rad=5, fill=255)
@@ -70,7 +67,7 @@ def func(filenametemp):
             blurred = img.filter(ImageFilter.GaussianBlur(20))
             # Paste blurred region and save result
             img.paste(blurred, mask=mask)
-        elif re.match(address1, str(i[1])) != None:
+        elif re.match(address1, str(i[1]).replace(' ','')) != None:
             mask = Image.new('L', img.size, 0)
             draw = ImageDraw.Draw(mask)
             rounded_rectangle(draw, i[0], rad=5, fill=255)
@@ -78,7 +75,7 @@ def func(filenametemp):
             blurred = img.filter(ImageFilter.GaussianBlur(20))
             # Paste blurred region and save result
             img.paste(blurred, mask=mask)
-        elif re.match(address2, str(i[1])) != None:
+        elif re.match(address2, str(i[1]).replace(' ','')) != None:
             mask = Image.new('L', img.size, 0)
             draw = ImageDraw.Draw(mask)
             rounded_rectangle(draw, i[0], rad=5, fill=255)
@@ -86,7 +83,7 @@ def func(filenametemp):
             blurred = img.filter(ImageFilter.GaussianBlur(20))
             # Paste blurred region and save result
             img.paste(blurred, mask=mask)
-        elif re.match(phone, str(i[1])) != None:
+        elif re.match(phone, str(i[1]).replace(' ','')) != None:
             mask = Image.new('L', img.size, 0)
             draw = ImageDraw.Draw(mask)
             rounded_rectangle(draw, i[0], rad=5, fill=255)
@@ -94,7 +91,23 @@ def func(filenametemp):
             blurred = img.filter(ImageFilter.GaussianBlur(20))
             # Paste blurred region and save result
             img.paste(blurred, mask=mask)
-        elif re.match(email, str(i[1])) != None:
+        elif re.match(email, str(i[1]).replace(' ','')) != None:
+            mask = Image.new('L', img.size, 0)
+            draw = ImageDraw.Draw(mask)
+            rounded_rectangle(draw, i[0], rad=5, fill=255)
+            # Blur image
+            blurred = img.filter(ImageFilter.GaussianBlur(20))
+            # Paste blurred region and save result
+            img.paste(blurred, mask=mask)
+        elif re.match(carNum, str(i[1]).replace(' ','')) != None:
+            mask = Image.new('L', img.size, 0)
+            draw = ImageDraw.Draw(mask)
+            rounded_rectangle(draw, i[0], rad=5, fill=255)
+            # Blur image
+            blurred = img.filter(ImageFilter.GaussianBlur(20))
+            # Paste blurred region and save result
+            img.paste(blurred, mask=mask)
+        elif re.match(carNum2, str(i[1]).replace(' ','')) != None:
             mask = Image.new('L', img.size, 0)
             draw = ImageDraw.Draw(mask)
             rounded_rectangle(draw, i[0], rad=5, fill=255)
