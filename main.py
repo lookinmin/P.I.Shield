f80020e399a5b03c63acb3b8e261e764f0178c05
JongWon <3338761@naver.com>
2022-11-04 23:08:03 +0900

    api 이미지 다운로드 구현

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import cv2
-import numpy as np
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 import easyocr
-import matplotlib.pyplot as plt
 from PIL import ImageFont, ImageDraw, Image, ImageFilter
 import re
 from fastapi import UploadFile
 from datetime import datetime
+import hashlib
 # async / await 비동기 처리가능
 app = FastAPI()
 
@@ -23,8 +21,8 @@ def func(filenametemp):
         draw.polygon(temp_xy, fill=fill)
 
     # 텍스트 찾는 AI
-    text_detect_result = reader.detect(filenametemp, min_size=3, text_threshold=0.1, low_text=0.4, link_threshold=0.2,
-                               width_ths=0.5) #최소사이즈, 텍스트 최소인식률, 작아질수록 인식 업 , ,박스 합쳐지는 범위
+    text_detect_result = reader.detect(filenametemp, min_size=1, text_threshold=0.1, low_text=0.4, link_threshold=0.1,
+                               width_ths=0.3) #최소사이즈, 텍스트 최소인식률, 작아질수록 인식 업 , ,박스 합쳐지는 범위
     # 찾은 텍스트를 txt 포맷으로 변경
     result = reader.recognize(filenametemp, text_detect_result[0][0],text_detect_result[1][0])
     print("AI 작업 완료")
@@ -48,7 +46,7 @@ def func(filenametemp):
     for i in result:
         # print(i[0],i[1],i[2])
         # 디텍션 된 텍스트가 전체 크기의 1/256보다 작다면 그냥 블러처리
-        if (img.size[0]*img.size[1])//256>(i[0][1][0]-i[0][0][0])*(i[0][2][1]-i[0][1][1]):
+        if (img.size[0]*img.size[1])//1024>(i[0][1][0]-i[0][0][0])*(i[0][2][1]-i[0][1][1]):
             rounded_rectangle(draw, i[0], fill=255)
         else:
             if re.match(registNum, str(i[1]).replace(" ",'')) != None:
@@ -75,19 +73,22 @@ def func(filenametemp):
                               temp_xy[(k+1)%4]], fill="blue", width=1)
                 # draw.text((int((x + x + w) / 2), y - 2), str(i[1]), font=font, fill='red')
     # Blur image
-    blurred = img.filter(ImageFilter.GaussianBlur(10))
+    blurred = img.filter(ImageFilter.GaussianBlur(20))
     # Paste blurred region and save result
     img.paste(blurred, mask=mask)
-    img.save('modify'+filenametemp,'jpeg')
+    img.save(filenametemp,'jpeg')
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
     now = datetime.now()
     current_time = now.strftime("%d%H%M%S")
-    print(current_time)
-    temp_file = open(current_time+'.jpg','wb+')
+    hashed_name=hashlib.md5(current_time.encode('utf-8')).hexdigest()
+    temp_file = open('image/'+str(hashed_name)+'.jpg','wb+')
     temp_file.write(file.file.read())
-    func(current_time+'.jpg')
+    func('image/'+str(hashed_name)+'.jpg')
 
+    return {"fileurl": str(hashed_name)+'.jpg'}
 
-    return {"filename": file.filename+current_time+'.jpg'}
+@app.get("/{filename}")
+async def downloadfile(filename:str):
+    return FileResponse(f"image/{filename}")
